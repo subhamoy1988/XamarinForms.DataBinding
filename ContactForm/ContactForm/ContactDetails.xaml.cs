598e482243566e62cbd2740620d49934e7d0ca46
unknown <sburman@USHYDSBURMAN1.us.deloitte.com>
2015-12-20 21:15:44 +0530

    Initial commit

---
+++ ContactForm/ContactForm/ContactDetails.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ContactForm
+{
+    public partial class ContactDetails : ContentPage
+    {
+        readonly Person personObject;
+        public ContactDetails(Person person)
+        {
+            InitializeComponent();
+            personObject = person;
+            headshot.Source = ImageSource.FromResource("ContactForm.Images." + person.HeadshotUrl);
+
+            nameEntry.Text = person.Name;
+            emailEntry.Text = person.Email;
+            birthday.Date = person.Dob;
+            favoriteSwitch.IsToggled = person.IsFavorite;
+            genderPicker.SelectedIndex = (person.Gender == Gender.Male) ? 0 : 1;
+
+            // Handle changes
+            //nameEntry.TextChanged += (sender, e) => person.Name = nameEntry.Text;
+            emailEntry.TextChanged += (sender, e) => person.Email = emailEntry.Text;
+            birthday.DateSelected += (sender, e) => person.Dob = birthday.Date;
+            favoriteSwitch.Toggled += (sender, e) => person.IsFavorite = favoriteSwitch.IsToggled;
+            genderPicker.SelectedIndexChanged += (sender, e) => person.Gender = genderPicker.SelectedIndex == 0 ? Gender.Male : Gender.Female;
+        }
+
+        void OnShow(object sender, EventArgs e)
+        {
+            this.DisplayAlert("Selected Contact", personObject.ToString(), "OK");
+        }
+    }
+}
---
+++ ContactForm/ContactForm/SImpsonFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactForm
+{
+    public enum Gender { Male, Female };
+    public class Person
+    {
+        public string Name { get; set; }
+        public string HeadshotUrl { get; set; }
+        public string Email { get; set; }
+        public DateTime Dob { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsFavorite { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name={0}, Email={1}, Dob={2}, Gender={3}, IsFavorite={4}", this.Name, this.Email, this.Dob, this.Gender, this.IsFavorite);
+        }
+    }
+    public class SimpsonFactory
+    {
+        public static Person GetPerson()
+        {
+            return new Person
+            {
+                Name = "Homer Simpson",
+                Email = "homer@gmail.com",
+                Gender = Gender.Male,
+                HeadshotUrl = "Homer.gif",
+                IsFavorite = false,
+                Dob = new DateTime(1999,12,1)
+            };
+        }
+
+    }
+}
